735b4d23fa333836f6327e72e1698879174f4cae
Dmitry <dimonvechorko@gmail.com>
2023-02-18 23:47:49 +0300

    Updated calculator and app for it

--- First tasks/src/Calculator.java
+++ First tasks/src/Calculator.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 public class Calculator {
-        public static int calculator() {
+    public double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+    public double difference(double num1, double num2) {
+        return num1 - num2;
+    }
+    public double multiplication(double num1, double num2){
+        return num1 * num2; }
+    public double division(double num1, double num2){
+        if(num2 != 0){return num1 / num2;}
+        else return 0;
+    }
+    public void calculate() {
         Scanner input = new Scanner(System.in);
+        Calculator calculator = new Calculator();
         double num1, num2;
         System.out.println("Insert the first number");
         num1 = input.nextDouble();
@@ -11,42 +24,22 @@ public class Calculator {
         int x = input.nextInt();
         switch (x) {
             case 1:
-                System.out.println(sum(num1,num2));
+                System.out.println(calculator.sum(num1,num2));
                 break;
             case 2:
-                System.out.println(difference(num1,num2));
+                System.out.println(calculator.difference(num1,num2));
                 break;
             case 3:
-                System.out.println(multiplication(num1,num2));
+                System.out.println(calculator.multiplication(num1,num2));
                 break;
             case 4:
-                System.out.println(division(num1,num2));
+                if (calculator.division(num1,num2)!=0) {
+                    System.out.println(calculator.division(num1, num2));
+                }
+                else{
+                    System.out.println("Error ( division by zero )");
+                }
                 break;
         }
-            System.out.println("If you want to continue insert 1");
-            int c = input.nextInt();
-            if (c == 1) {
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-    private static double sum(double num1, double num2) {
-        return num1 + num2;
-    }
-    private static double difference(double num1, double num2) {
-        return num1 - num2;
-    }
-
-    private static double multiplication(double num1, double num2){
-            return num1 * num2; }
-
-    private static double division(double num1, double num2){
-            return num1 / num2; }
-
-    public static void main(String[] args) {
-        if(calculator()==1)
-        calculator();
     }
 }

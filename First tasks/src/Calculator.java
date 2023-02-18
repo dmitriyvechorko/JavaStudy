48f2027f8b0d6f0620c176fb444f3d7037f0c001
Dmitry <dimonvechorko@gmail.com>
2023-02-18 23:11:32 +0300

    Updated calculator

--- First tasks/src/Calculator.java
+++ First tasks/src/Calculator.java
@@ -2,29 +2,25 @@ import java.util.Scanner;
 public class Calculator {
         public static int calculator() {
         Scanner input = new Scanner(System.in);
-        int num1, num2;
+        double num1, num2;
         System.out.println("Insert the first number");
-        num1 = input.nextInt();
+        num1 = input.nextDouble();
         System.out.println("Insert the second number");
-        num2 = input.nextInt();
+        num2 = input.nextDouble();
         System.out.println("1 - sum" + "\n" + "2 - difference" + "\n" + "3 - multiplication" + "\n" + "4 - division");
         int x = input.nextInt();
         switch (x) {
             case 1:
-                int sum = num1 + num2;
-                System.out.println("The result is: " + sum);
+                System.out.println(sum(num1,num2));
                 break;
             case 2:
-                int difference = num1-num2;
-                System.out.println("The result is: " + difference);
+                System.out.println(difference(num1,num2));
                 break;
             case 3:
-                int multiplication = num1*num2;
-                System.out.println("The result is: " + multiplication);
+                System.out.println(multiplication(num1,num2));
                 break;
             case 4:
-                int division = num1/num2;
-                System.out.println("The result is: " + division);
+                System.out.println(division(num1,num2));
                 break;
         }
             System.out.println("If you want to continue insert 1");
@@ -36,6 +32,19 @@ public class Calculator {
                 return 0;
             }
         }
+    private static double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+    private static double difference(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    private static double multiplication(double num1, double num2){
+            return num1 * num2; }
+
+    private static double division(double num1, double num2){
+            return num1 / num2; }
+
     public static void main(String[] args) {
         if(calculator()==1)
         calculator();

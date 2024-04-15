a1ea8c4fe63c52893a18a9cf7d0e0e59ce950964
Dimon <dimonvechorko@gmail.com>
2024-04-15 18:36:00 +0300

    Fixing Patient

--- Lesson7/src/Doctors/Patient.java
+++ Lesson7/src/Doctors/Patient.java
@@ -7,15 +7,7 @@ class Patient {
     public Patient(int treatmentPlan) {
         this.treatmentPlan = treatmentPlan;
     }
-    //public void assignDoctor() {
-    //    if (treatmentPlan == 1) {
-    //        doctor = new Surgeon();
-    //    } else if (treatmentPlan == 2) {
-    //        doctor = new Dentist();
-    //    } else {
-    //        doctor = new Therapist();
-    //    }
-    // }
+
     public void receiveTreatment() {
         doctor.treat();
     }
